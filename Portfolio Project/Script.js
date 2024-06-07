d11cea5358267be44d7cb7f457d0ae95b6964c23
nfikri9 <nabilfikri.business@gmail.com>
2024-06-07 21:20:46 +0800

    Updating 3 files

--- Portfolio Project/Script.js
+++ Portfolio Project/Script.js
@@ -1,8 +1,12 @@
 function displayOutput(){
     document.getElementById("display").style.display = "initial";
     document.getElementById("display-full-name").innerText = document.getElementById("fullname").value;
+    document.getElementById("display-ic").innerText = document.getElementById("ic").value;
     document.getElementById("display-dob").innerText = document.getElementById("dob").value;
     document.getElementById("display-race").innerText = document.getElementById("race").value;
     /* These statements need improvement */
-    document.getElementById("display-gender").innerText = document.getElementById("male").value;
+    for (i = 0; i < document.getElementsByName("gender").length; i++) {
+        if (document.getElementsByName("gender")[i].checked)
+        document.getElementById("display-gender").innerText = document.getElementsByName("gender")[i].value;
+    }
 }
